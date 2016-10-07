5a731c447c36db14f2743f3407a80914bf8faf0f
Dmitriy Babkov <dsbabkov@gmail.com>
2016-10-07 22:23:55 +0300

    Выполнены задания 2 и 3 из лабораторной 4

--- lab4/other.cpp
+++ lab4/other.cpp
@@ -31,3 +31,77 @@ void VarArgs(int arg1,...)
 */
 
 
+
+void IncByPointer(int *val)
+{
+    ++(*val);
+}
+
+void IncByReference(int &val)
+{
+    ++val;
+}
+
+int IncByValue(int val)
+{
+    return ++val;
+}
+
+void Swap(int *left, int *right)
+{
+    int tmp = *left;
+    *left = *right;
+    *right = tmp;
+
+}
+
+void Swap(int &left, int &right)
+{
+    int tmp = left;
+    left = right;
+    right = tmp;
+}
+
+int Min(int **array, int rows, int cols)
+{
+    int result = array[0][0];
+    for (int i = 0; i < rows; ++i){
+        for (int j = 0; j < cols; ++j){
+            if (array[i][j] < result){
+                result = array[i][j];
+            }
+        }
+    }
+    return result;
+}
+
+int Min(int array[][M], int rows)
+{
+    return Min(&array[0][0], rows * M);
+}
+
+int Min(int *array, int elementCount)
+{
+    int result = *array;
+
+    for (int *p = array, *end = p + elementCount * M; p != end; ++p){
+        if (*p < result){
+            result = *p;
+        }
+    }
+
+    return result;
+}
+
+int MyStrCmp(const char *left, const char *right)
+{
+    if (!left || !right || right == left){
+        return 0;
+    }
+
+    while (*left == *right && *left && *right){
+        ++left;
+        ++right;
+    }
+    return *left - *right;
+}
--- lab4/other.h
+++ lab4/other.h
@@ -1,3 +1,18 @@
 
 //Прототипы используемых в данном задании функций:
 
+const int N = 4;
+const int M = 5;
+
+int IncByValue(int val);
+void IncByPointer(int *val);
+void IncByReference(int &val);
+
+void Swap(int *left, int *right);
+void Swap(int &left, int &right);
+
+int Min (int **array, int rows, int cols);
+int Min (int array[][M], int rows);
+int Min (int *array, int elementCount);
+
+int MyStrCmp(const char *left, const char *right);
